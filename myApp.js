825591b0bf72a2108d73c8a5200ce3df5784684b
Glitch (sundhar811-mongodb-and-mongoose) <none>
2019-09-28 03:22:16 +0000

    Create a Model

--- myApp.js
+++ myApp.js
@@ -12,7 +12,7 @@
 // `mongoose`. Store your **mLab** database URI in the private `.env` file 
 // as `MONGO_URI`. Connect to the database using `mongoose.connect(<Your URI>)`
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGO_URI);
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 /** # SCHEMAS and MODELS #
 /*  ====================== */
@@ -38,8 +38,16 @@ mongoose.connect(process.env.MONGO_URI);
 // `default` values. See the [mongoose docs](http://mongoosejs.com/docs/guide.html).
 
 // <Your code here >
+let personSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  age: Number,
+  favoriteFoods: [String]
+});
 
-var Person /* = <Your Model> */
+let Person = mongoose.model('Person', personSchema);
 
 // **Note**: Glitch is a real server, and in real servers interactions with
 // the db are placed in handler functions, to be called when some event happens
